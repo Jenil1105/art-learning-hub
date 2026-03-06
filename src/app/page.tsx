d3312837d61dc57c00e19c91239601a4db32440d
Jenil1105 <unselfstudy@gmail.com>
2026-03-06 22:14:44 +0530

    Add featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <FeaturedCourses />
     </>
   );
 }
